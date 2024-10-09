d4cfaa05a190b428f574b9bb3f7c443bfc2a8af9
ViruPatel3011 <pviral3011@gmail.com>
2024-10-09 10:58:33 +0530

    @Input and @Output decorator

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,11 @@
-import { Component, signal, computed } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+import {
+  Component,
+  computed,
+  EventEmitter,
+  Input,
+  input,
+  Output,
+} from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -11,16 +15,27 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
-  imagePath=computed(()=> 'assets/users/' + this.selectedUser().avatar)
+  // Decorator based approach
 
-  // bcz of getter we are using this like property not method
-  // get imagePath() {
-  //   return 'assets/users/' + this.selectedUser.avatar;
-  // }
+  @Input({ required: true }) id!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+  @Output() select = new EventEmitter();
+
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  }
+
+  // Signal  based approach
+
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+
+  // imagePath = computed(()=>{
+  //   return 'assets/users/' + this.avatar();
+  // });
 
   onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+    this.select.emit(this.id);
   }
 }
